586a7f78f1ceb9a82e8ce70fa9691a5ec0dad4ff
Fernanda Silvestre <silvestredasilvafernanda@gmail.com>
2025-02-05 21:55:00 +0000

    New lesson

---
+++ Python_Conditionals_and_Loops/aula13.py
@@ -0,0 +1,13 @@
+for c in range (1,6):
+    print('Oi!')
+    #mostra 5 vezes, porque o python ignora o último número.
+    #que que mostre 6 vezes, coloco ( 1,7)
+print('-='*11)
+
+for c in range (6,0,-1):
+    print(c)
+print('-='*11)
+
+for c in range (0,7,2):
+    print(c)
+print('-='*11)
