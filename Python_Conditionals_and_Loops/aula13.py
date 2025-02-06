8ec9e5848046b7c7fea0d58ae0913b9d3ec92c8f
Fernanda Silvestre <silvestredasilvafernanda@gmail.com>
2025-02-06 16:57:42 +0000

    New lesson

--- Python_Conditionals_and_Loops/aula13.py
+++ Python_Conditionals_and_Loops/aula13.py
@@ -1,13 +1,26 @@
 for c in range (1,6):
     print('Oi!')
-    #mostra 5 vezes, porque o python ignora o último número.
-    #que que mostre 6 vezes, coloco ( 1,7)
+    #mostra 5 vezes o 'oi', porque o python ignora o último número.
+    #se eu quiser mostrar 6 vezes, escrevo (1,7)
 print('-='*11)
 
 for c in range (6,0,-1):
     print(c)
+    #vai mostrar os números em ordem decrescente pelo '-1' no final da linha
 print('-='*11)
 
 for c in range (0,7,2):
     print(c)
+    #vai mostrar os números pulando de 2 em 2 pelo '2' no final da linha
+print('-='*11)
+
+n = int(input('Digite um número: '))
+for c in range (0,n):
+    print(c)
+print('-='*11)
+
+n = int(input('Digite um número: '))
+for c in range (0,n+1):
+    print(c)
+    #para ir até o número digitado
 print('-='*11)
