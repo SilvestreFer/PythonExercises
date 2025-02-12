c70bad6093a5a9c0648dfd69b808cf019e0c4975
Fernanda Silvestre <silvestredasilvafernanda@gmail.com>
2025-02-12 15:30:17 +0000

    Started a new exercice

---
+++ Python_Conditionals_and_Loops/ex056.py
@@ -0,0 +1,11 @@
+pessoa = []
+
+for i in range(4):
+    print(f'Sobre a pessoa {i+1}')
+    nome=input('Nome: ')
+    idade=int(input('Idade: '))
+    sexo=input('Sexo: ')
+
+#media_idade
+#homem_mais_velho
+#mulheres_menos_20
